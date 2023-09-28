d0097dceca7cc5e0ef21cd5910ea7c570f34ca41
Owen <owen@persistance>
2023-09-29 07:58:15 +1000

    Interleave markup and git diffs

--- src/git_blog_clj/core.clj
+++ src/git_blog_clj/core.clj
@@ -1,9 +1,9 @@
 (ns git-blog-clj.core
   (:gen-class)
   (:require
-   [clj-jgit.internal :as gi]
    [clj-jgit.porcelain :as gp]
-   [clj-jgit.querying :as gq]))
+   [clj-jgit.querying :as gq]
+   [clojure.string :as string]))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -17,16 +17,70 @@
   "
   (gp/load-repo ".git"))
 
-(gi/resolve-object r "26ced4b9468d769a347102684e6b5513ee0d37a7")
+(def repo-data
+  (->>
+   (map gq/changed-files-with-patch (repeat r) (gq/rev-list r))
+   (interpose "\n\n\n")
+   (apply str)
+   string/split-lines))
 
-(println
- (gq/changed-files-with-patch
-  r
-  (second
-   ;; Turns out to be a poor man's rev-list
-   (keys
-    (gq/build-commit-map r
-                         (gi/new-rev-walk r))))))
+(spit "my-text.txt"
+      "Nothing\nYet\n|   :main ^:skip-aot git-blog-clj.core\nHehe")
 
-(run! println
-      (map gq/changed-files-with-patch (repeat r) (gq/rev-list r)))
+(def markup-data
+  (->> (slurp "my-text.txt")
+       string/split-lines))
+
+(defn output
+  [markup-data repo-data]
+  (loop [acc
+         []
+
+         m
+         markup-data
+
+         r
+         repo-data]
+    (let [[m' & m+]
+          m
+
+          [r' & r+]
+          r
+
+          matchable-m?
+          (= (first m') \|)]
+      (cond
+        ;; Case 0; we're finished. Return the accumulator
+        (and (empty? r) (empty? m))
+        acc
+
+        ;; Case 1; We've consumed r -> keep consuming m
+        (empty? r)
+        (recur (conj acc m') m+ r)
+
+        ;; Case 2; We've consumed m -> keep consuming r
+        (empty? m)
+        (recur (conj acc r') m r+)
+
+        ;; Case 3; we're adding lines from the markup file until we find a new thing to match on.
+        (not matchable-m?)
+        (recur
+         (conj acc m') m+ r)
+
+        ;; Case 4; we're looking for a match and find one.
+        (= r' (string/replace-first m' "|" ""))
+        (recur (conj acc r') m+ r+)
+
+        ;; Case 5; we're waiting for a match but can't possibly find it. Dump
+        (empty? r)
+        (recur (conj acc m') m+ r)
+
+        ;; Case 6; we're waiting for a match and don't see it yet.
+        :else
+        (recur (conj acc r') m r+)))))
+
+(spit "out.txt"
+      (->>
+       (output markup-data repo-data)
+       (interpose "\n")
+       (apply str)))
