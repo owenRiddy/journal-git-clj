e1a0e3caa32edcb28b070bf0f64d0d483ee60bde
Owen <owen@persistance>
2023-09-29 22:33:32 +1000

    Make it better

--- src/git_blog_clj/core.clj
+++ src/git_blog_clj/core.clj
@@ -17,19 +17,38 @@
   "
   (gp/load-repo ".git"))
 
+(def skip-commits
+  #{"5ec8855d6c3455087eb556578a07892f8005ad94"})
+
+(defn banned-sha1-hash?
+  [^org.eclipse.jgit.revwalk.RevCommit rev]
+  (->> rev
+       .getName
+       (contains? skip-commits)))
+
+(defn to-diff-block
+  [idx file]
+  (->>
+   (concat ["" (str "# Commit " (inc idx)) "```diff"] file ["````" "" ""])
+   (into [])))
+
 (def repo-data
   (->>
-   (map gq/changed-files-with-patch (repeat r) (gq/rev-list r))
+   (gq/rev-list r)
+   (remove banned-sha1-hash?)
    reverse
-   (interpose "\n```\n\n\n# Next Commit\n```diff\n")
-   reverse
-   (into ["```"])
-   reverse
-   (apply str)
-   string/split-lines))
+   (map gq/changed-files-with-patch (repeat r))
+   ;; `gq/changed-files-with-patch` does not return strings, it evaluates to
+   ;; some sort of quasi-string object that breaks split-lines, somehow. Java
+   ;; folk, at it again with their wacky ideas!
+   (map str)
+   (filter seq)
+   (map string/split-lines)
+   (map-indexed to-diff-block)
+   flatten))
 
 (spit "my-text.txt"
-      "Nothing\nYet\n|   :main ^:skip-aot git-blog-clj.core\nChecking the logic\n|+(def repo-data\n\nThe order of the commits was backwards. Dunno if it is consistently backwards or not though.\n\n||FIN.\n\n")
+      "Nothing\n\nYet\n|   :main ^:skip-aot git-blog-clj.core\nChecking the logic\n|+(def repo-data\n\nThe order of the commits was backwards. Dunno if it is consistently backwards or not though.\n\n||FIN.\n\n")
 
 (def markup-data
   (->> (slurp "my-text.txt")
@@ -39,6 +58,9 @@
   [markup-data repo-data]
   (loop [acc
          []
+
+         state ; free in-diff
+         :free
 
          m
          markup-data
@@ -60,32 +82,33 @@
 
         ;; Case 1; We've consumed r -> keep consuming m
         (empty? r)
-        (recur (conj acc m') m+ r)
+        (recur (conj acc m') :free m+ r)
 
         ;; Case 2; We've consumed m -> keep consuming r
         (empty? m)
-        (recur (conj acc r') m r+)
+        (recur (conj acc r') :free m r+)
 
         ;; Case 3; we're adding lines from the markup file until we find a new thing to match on.
-        ;; Case 3+; We're just about to finish case 3, make sure to get the ``` correct.
         (not matchable-m?)
-        (if (= (ffirst m+) \|)
-          (recur
-           (into acc [m' "" "```diff"]) m+ r)
-          (recur
-           (conj acc m') m+ r))
+        (recur
+         (conj acc m') state m+ r)
 
-        ;; Case 4; we're looking for a match and find one.
+        ;; Case 4; we're looking for a match and find one. Since we matched a
+        ;; line of code we must be interrupting a diff.
         (= r' (string/replace-first m' "|" ""))
-        (recur (into acc [r' "```"]) m+ r+)
+        (recur (into acc [r' "```"]) :in-diff m+ r+)
 
         ;; Case 5; we're waiting for a match but can't possibly find it. Dump
         (empty? r)
-        (recur (conj acc m') m+ r)
+        (recur (conj acc m') state m+ r)
 
         ;; Case 6; we're waiting for a match and don't see it yet.
         :else
-        (recur (conj acc r') m r+)))))
+        (let [new-items
+              (if (= state :in-diff)
+                ["" "```diff" r']
+                [r'])]
+          (recur (into acc new-items) :free m r+))))))
 
 (spit "out.txt"
       (->>
